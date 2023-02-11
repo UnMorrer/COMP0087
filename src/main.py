74623db8b8cbacb833bad09e28d15bc683426537
Marcell O <omarcell@yahoo.com>
2023-02-11 13:40:59 +0000

    Renamed main.py

--- src/main.py
+++
@@ -1,4 +0,0 @@
-# Placeholder for future automation runs etc.
-
-if __name__ == "__main__":
-    pass
---
+++ src/main_chatGPT.py
@@ -0,0 +1,11 @@
+import src.chatGPT.chatGPT_proxied as chatgpt
+
+def main():
+    """
+    Runner/orchestration script for chatGPT scraping
+    """
+
+
+
+if __name__ == "__main__":
+    main()
