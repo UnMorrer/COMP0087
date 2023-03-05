7f74233dbd9ba90b1d828f5316b04c3557c48b4d
Marcell O <omarcell@yahoo.com>
2023-03-05 12:53:54 +0000

    Created masking strategy

--- src/tokenization/ner_masking.py
+++ src/tokenization/ner_masking.py
@@ -1,2 +1,27 @@
 # Previous work: ner.py
 # Script that masks named entities in generated text
+
+import transformers as trfs
+
+# Example configuration
+# ner = trfs.pipeline("ner", grouped_entities=True) # TODO: Specify model revision + framework
+# resp = ner("This is a test text to see how much this encoding can handle. I started working for Dr. Hammerschmidt in February 2023 for a salary of £60,000 ($75,000). My e-mail address is hammerschmidt@gmail.com")
+
+# This extracts: LOCation, MISCellanous, ORGanization, PERson
+# We are missing: Time/Month, Money, Number, Dr, Percent
+# Above: Dr. XY is identified as PERSON
+
+# Plan: Month/Time -> unique format, can be found
+# Money: Currency (dollar/$) -> can be found
+# Number and percent (digits%) -> unique format, can be found
+# Email address (..@.. .com) -> unique format, can be found
+
+# Re-code human essays:
+# @MONTH + @DATE + @TIME -> @TIME (or @MISC)
+# @CAPS + @PERSON + @DR -> @PER (match NER label)
+# @CITY + @STATE + @LOCATION -> @LOC (match NER label)
+# @ORGANIZATION -> @ORG (match NER label)
+# @MONEY + @PERCENT + @NUM -> @NUM
+# @EMAIL -> @EMAIL
+# Check: @MISC out of NER
+# '@MONTH', '@CAPS', '@MONEY', '@PERSON', '@PERCENT', '@STATE', '@LOCATION', '@NUM', '@DATE', '@EMAIL', '@CITY', '@DR', '@ORGANIZATION', '@TIME'
