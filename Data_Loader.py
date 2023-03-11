46c99272e9cb3b0614b1f358b67a785923efd8df
Zythume <t.du.crest@hotmail.fr>
2023-03-11 10:50:43 +0000

    DataLoader

--- Data_Loader.py
+++ Data_Loader.py
@@ -14,4 +14,14 @@ class TextDataset(Dataset):
         question = self.df.iloc[idx]['question']
 
         return {'input_ids': text, 'label': label, 'question': question}
+class DataLoader:
+    def __init__(self, dataset, batch_size=1, shuffle=False):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
 
+    def __iter__(self):
+        return DataLoader(self.dataset, batch_size=self.batch_size, shuffle=self.shuffle).__iter__()
+
+    def __len__(self):
+        return len(self.dataset) // self.batch_size
