7ad38f4df7f5db4914410f1f0ca5d563290d6c8b
Marcell O <omarcell@yahoo.com>
2023-03-24 21:58:48 +0000

    Started working on utils.py

--- src/evaluation/utils.py
+++ src/evaluation/utils.py
@@ -1,1 +1,15 @@
 # Utilities for model evaluation
+
+def compare_predictions(
+        probs,
+        prob_type="fake",
+        threshold=0.5):
+    """
+    Function evaluating prediction accuracy.
+
+    Inputs:
+
+    Returns:
+    
+    """
+    pass
