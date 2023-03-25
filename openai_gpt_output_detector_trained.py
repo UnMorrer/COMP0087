b57698991fbf107196b7f20a134a80f9216d75b1
Marcell O <omarcell@yahoo.com>
2023-03-25 15:58:52 +0000

    Fixed ImportError

--- openai_gpt_output_detector_trained.py
+++ openai_gpt_output_detector_trained.py
@@ -2,6 +2,7 @@
 
 # Base packages
 import torch
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from transformers import RobertaForSequenceClassification, RobertaTokenizer, Trainer, TrainingArguments
